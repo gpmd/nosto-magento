627accf8f2061411b41b73e8541db70ccb92e0a8
Sam Ban <sam@gpmd.co.uk>
2018-08-08 16:17:58 +0100

    nodeps

---
+++ lib/Dotenv/Exception/ExceptionInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+
+
+/**
+ * This is the exception interface.
+ */
+interface Dotenv_Exception_ExceptionInterface
+{
+    //
+}
---
+++ lib/Dotenv/Exception/InvalidCallbackException.php
@@ -0,0 +1,11 @@
+<?php
+
+
+
+/**
+ * This is the invalid callback exception class.
+ */
+class Dotenv_Exception_InvalidCallbackException extends InvalidArgumentException implements Dotenv_Exception_ExceptionInterface
+{
+    //
+}
